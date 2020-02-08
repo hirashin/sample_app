8e5ffd1b0591fc153cc341cda882592aa7939f36
hirashin <himaraya_0506@yahoo.co.jp>
2020-02-08 21:35:44 +0900

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -5,7 +5,9 @@ ruby '2.7.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.1'
-# Use sqlite3 as the database for Active Record
+gem 'bootstrap-sass', '3.3.7'
+# Ruby Sassが終了したため追加
+gem 'sassc'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,27 +8,21 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   # 3.4.4 演習
   test "should get root" do
-    get root_url
+    get root_path
     assert_response :success
     assert_select "title", "#{@base_title}"
   end
 
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-    # 3.4.1 viewのタイトルをテストする
-    assert_select "title", "#{@base_title}"
-  end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     # 3.4.1 viewのタイトルをテストする
     assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     # 3.4.1 viewのタイトルをテストする
     assert_select "title", "About | #{@base_title}"
@@ -36,7 +30,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   # 3.4.3 演習
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
